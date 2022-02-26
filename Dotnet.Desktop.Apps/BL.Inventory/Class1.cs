f50392a244e25fce5c0f56ed01d28b285c0b68ce
aditya_deshpande <aditya.deshpande@outlook.in>
2022-02-26 22:11:27 +0530

    console - sample inventory app started

---
+++ Dotnet.Desktop.Apps/BL.Inventory/Class1.cs
@@ -0,0 +1,43 @@
+﻿namespace BL.Inventory
+{
+    public class Inventory
+    {
+        public List<InventoryItem> items { get; set; }
+
+        public Inventory()
+        {
+            items = new List<InventoryItem>();
+            FillInventoryWithDefault();
+        }
+
+        public void FillInventoryWithDefault()
+        {
+            items.Add(new InventoryItem { id = 1, name = "Bread", description = "Samrat White Bread", purchaseValue = 24.00, sellValue = 30.00, unit = 15 });
+            items.Add(new InventoryItem { id = 2, name = "Milk", description = "Amul Cow Milk 0.5L", purchaseValue = 24.00, sellValue = 26.00, unit = 20 });
+            items.Add(new InventoryItem { id = 3, name = "Butter", description = "Amul 220gm", purchaseValue = 40.00, sellValue = 45.00, unit = 10 });
+        }
+
+        public void AddItem(InventoryItem item)
+        {
+            var invItem = items.FirstOrDefault(x => x.id == item.id);
+            if (invItem != null)
+            {
+                invItem.unit += 1;
+            }
+            else
+            {
+                items.Add(item);
+            }
+        }
+    }
+
+    public class InventoryItem
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int unit { get; set; }
+        public string description { get; set; }
+        public double purchaseValue { get; set; }
+        public double sellValue { get; set; }
+    }
+}
---
+++ Dotnet.Desktop.Apps/Console.Inventory/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BL.Inventory;
+
+Console.WriteLine("");
+Console.WriteLine("Hello, World!");
+
+
+
+void DisplayInventory(Inventory inventory) {
+    for (int i = 0; i < 100; i++) {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.Write(".");
+    }
+    Console.ResetColor();
+    foreach()
+    Console.WriteLine();
+}
